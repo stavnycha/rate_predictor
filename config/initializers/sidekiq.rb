c2e7b1deea7dd0433c3d9347f0caf2be40bb5ab2
Myroslava Stavnycha <myroslava@perfectline.co>
2017-10-28 09:43:16 +0300

    fix sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 unless Rails.env.development?
   Sidekiq.configure_server do |config|
-    config.redis = { size: 20, url: ENV['REDISCLOUD_URL'] || 'redis://127.0.0.1:6379' }
+    config.redis = { size: 20, url: ENV['REDIS_URL'] || 'redis://127.0.0.1:6379' }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { size: 2, url: ENV['REDISCLOUD_URL'] || 'redis://127.0.0.1:6379' }
+    config.redis = { size: 2, url: ENV['REDIS_URL'] || 'redis://127.0.0.1:6379' }
   end
 end

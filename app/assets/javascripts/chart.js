e0aec8b9adc09e7346d65f706468a14065583387
Myroslava Stavnycha <myroslava@perfectline.co>
2017-10-28 09:06:36 +0300

    better chart

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -1,16 +1,62 @@
-function PredictionChart(container, history, predicted){
-  var data = {
-    series: [
-      history.concat(new Array(predicted.length)),
-      new Array(history.length).concat(predicted)
-    ]
-  };
+function PredictionChart(container, labels, history, predicted){
+  var green = 'rgb(75, 192, 192)'
+  var blue = 'rgb(54, 162, 235)'
+
+  var context = document.getElementById(container).getContext('2d');
 
   var options = {
-    showPoint: false,
-    lineSmooth: false,
-    height: 500
+    responsive: true,
+    title:{
+      display:true,
+      text:'Exchange rate'
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false,
+    },
+    hover: {
+      mode: 'nearest',
+      intersect: true
+    },
+    scales: {
+      xAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Date'
+        }
+      }],
+      yAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Rate'
+        }
+      }]
+    }
   };
 
-  new Chartist.Line(container, data, options);
+  var data = {
+    labels: labels,
+    datasets: [{
+      label: 'History rates',
+      fill: false,
+      backgroundColor: blue,
+      borderColor: blue,
+      data: history
+    },{
+      label: 'Predicted rates',
+      fill: false,
+      borderDash: [5, 5],
+      backgroundColor: green,
+      borderColor: green,
+      data: new Array(history.length).concat(predicted)
+    }]
+  };
+
+  var chart = new Chart(context, {
+    type: 'line',
+    data: data,
+    options: options
+  })
 }
